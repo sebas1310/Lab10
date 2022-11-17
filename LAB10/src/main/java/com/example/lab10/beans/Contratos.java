f30ca3a5a6117c6472974e22b56e152bb2d1e955
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-16 20:41:33 -0500

    abigail contrato y credential beans

--- LAB10/src/main/java/com/example/lab10/beans/Contratos.java
+++ LAB10/src/main/java/com/example/lab10/beans/Contratos.java
@@ -1,4 +1,49 @@
 package com.example.lab10.beans;
 
 public class Contratos {
+    private String nroDeContrato;
+    private int idCliente;
+    private int divisa;
+    private int estado;
+    private int mesesEnEseEstado;
+
+    public String getNroDeContrato() {
+        return nroDeContrato;
+    }
+
+    public void setNroDeContrato(String nroDeContrato) {
+        this.nroDeContrato = nroDeContrato;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getDivisa() {
+        return divisa;
+    }
+
+    public void setDivisa(int divisa) {
+        this.divisa = divisa;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getMesesEnEseEstado() {
+        return mesesEnEseEstado;
+    }
+
+    public void setMesesEnEseEstado(int mesesEnEseEstado) {
+        this.mesesEnEseEstado = mesesEnEseEstado;
+    }
 }
