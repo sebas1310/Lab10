1de106673b1524ac7f415bce7ae84462ad8fb9bc
sebas1310 <a20203369@pucp.edu.pe>
2022-11-16 20:31:40 -0500

    clientes

--- LAB10/src/main/java/com/example/lab10/daos/DaoCliente.java
+++ LAB10/src/main/java/com/example/lab10/daos/DaoCliente.java
@@ -1,4 +1,14 @@
 package com.example.lab10.daos;
 
+import com.example.lab10.beans.Clientes;
+import com.example.lab10.beans.Credentials;
+
+
 public class DaoCliente {
+    public Credentials buscarUsuario(String numeroDocumento, String password){
+
+        Credentials credentials = new Credentials();
+        
+
+    }
 }
