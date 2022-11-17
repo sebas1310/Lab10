68206b07caa698616b7fb6fbdfd94879f5adbda9
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-17 18:10:05 -0500

    abigail cantidad contrato dto

---
+++ LAB10/src/main/java/com/example/lab10/Dtos/CantidadContratosDto.java
@@ -0,0 +1,22 @@
+package com.example.lab10.Dtos;
+
+public class CantidadContratosDto {
+    private int estados;
+    private int contratoXestado;
+
+    public int getEstados() {
+        return estados;
+    }
+
+    public void setEstados(int estados) {
+        this.estados = estados;
+    }
+
+    public int getContratoXestado() {
+        return contratoXestado;
+    }
+
+    public void setContratoXestado(int contratoXestado) {
+        this.contratoXestado = contratoXestado;
+    }
+}
