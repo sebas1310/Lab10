42c8063a61c381fd3b197323071976ee7dca984b
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-17 18:10:22 -0500

    abigail max expected dto

---
+++ LAB10/src/main/java/com/example/lab10/Dtos/MaxExpectedLossDto.java
@@ -0,0 +1,13 @@
+package com.example.lab10.Dtos;
+
+public class MaxExpectedLossDto {
+    private int resultado;
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+}
